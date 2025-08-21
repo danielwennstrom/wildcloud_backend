6bdfa1ba947cd0e573efe1bf3e4af94ead279ec4
Daniel Wennström <daniel@wennstrom.io>
2025-08-21 10:25:24 +0200

    init

---
+++ src/main/java/org/wildcloud/wildcloud_backend/WildcloudBackendApplication.java
@@ -0,0 +1,13 @@
+package org.wildcloud.wildcloud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcloudBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WildcloudBackendApplication.class, args);
+    }
+
+}
