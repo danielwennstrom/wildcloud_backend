25e75e0846965f6cd8bd5d8ef163baa2dddde943
Daniel Wennström <daniel@wennstrom.io>
2025-08-27 09:42:54 +0200

    Dotenv implemented
    (cherry picked from commit 721ff0b2c96317cdb369e09ec5aaf1316d28f9c9)
    
    # Conflicts:
    #	pom.xml
    #	src/main/resources/application.properties

--- src/main/java/org/wildcloud/wildcloud_backend/WildcloudBackendApplication.java
+++ src/main/java/org/wildcloud/wildcloud_backend/WildcloudBackendApplication.java
@@ -1,5 +1,6 @@
 package org.wildcloud.wildcloud_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -9,6 +10,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class WildcloudBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().systemProperties().ignoreIfMissing().directory("src/main/java/org/wildcloud/wildcloud_backend/config").load();
+        
         SpringApplication.run(WildcloudBackendApplication.class, args);
     }
 
