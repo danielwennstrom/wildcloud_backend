2033e5cb1bdd7c7aa0446c98a0840925da1cd380
Daniel Wennström <daniel@wennstrom.io>
2025-09-11 11:11:18 +0200

    Removed dotenv

--- src/main/java/org/wildcloud/wildcloud_backend/WildcloudBackendApplication.java
+++ src/main/java/org/wildcloud/wildcloud_backend/WildcloudBackendApplication.java
@@ -1,6 +1,5 @@
 package org.wildcloud.wildcloud_backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WildcloudBackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().systemProperties().ignoreIfMissing().directory("src/main/java/org/wildcloud/wildcloud_backend/config").load();
-        
         SpringApplication.run(WildcloudBackendApplication.class, args);
     }
 
