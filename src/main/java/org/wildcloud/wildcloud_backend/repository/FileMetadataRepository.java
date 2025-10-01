2e71c54217496d60be06654f8bec735cc5e888c4
danielwennstrom <daniel@wennstrom.io>
2025-10-01 13:02:54 +0200

    Removed unnecessary methods in repositories

--- src/main/java/org/wildcloud/wildcloud_backend/repository/FileMetadataRepository.java
+++ src/main/java/org/wildcloud/wildcloud_backend/repository/FileMetadataRepository.java
@@ -1,18 +1,11 @@
 package org.wildcloud.wildcloud_backend.repository;
 
-import org.springframework.data.r2dbc.repository.Modifying;
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.wildcloud.wildcloud_backend.entity.FileMetadata;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 public interface FileMetadataRepository extends ReactiveCrudRepository<FileMetadata, Long> {
     Flux<FileMetadata> findByImageIdIn(List<Long> imageIds);
-
-    @Modifying
-    @Query("DELETE FROM file_metadata WHERE image_id = :imageId")
-    Mono<Void> deleteByImageEntityId(Long imageId);
 }
--- src/main/java/org/wildcloud/wildcloud_backend/repository/ImageMetadataRepository.java
+++ src/main/java/org/wildcloud/wildcloud_backend/repository/ImageMetadataRepository.java
@@ -1,21 +1,11 @@
 package org.wildcloud.wildcloud_backend.repository;
 
-import org.springframework.data.r2dbc.repository.Modifying;
-import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.wildcloud.wildcloud_backend.entity.ImageMetadata;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 
 public interface ImageMetadataRepository extends ReactiveCrudRepository<ImageMetadata, Long> {
     Flux<ImageMetadata> findByImageIdIn(List<Long> imageIds);
-
-    @Modifying
-    @Query("DELETE FROM image_metadata WHERE image_entity_id = :imageId")
-    Mono<Void> deleteByImageEntityId(Long imageId);
-
-    Flux<ImageMetadata> findByCapturedAtBetween(OffsetDateTime start, OffsetDateTime end);
 }
